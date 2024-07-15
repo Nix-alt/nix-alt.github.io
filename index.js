40dc85b6b09d2b252291c74a12d3fbb9d0c2fedf
Nix-alt <167569462+Nix-alt@users.noreply.github.com>
2024-07-15 14:48:06 -0400

    test

---
+++ index.js
@@ -0,0 +1,13 @@
+alert('test');
+setTimeout(function(){
+    alert('\ttes')
+}, 2000);
+setTimeout(function(){
+    alert('\tte')
+}, 2000);
+setTimeout(function(){
+    alert('\tt')
+}, 2000);
+setTimeout(function(){
+    alert('\t')
+}, 2000);
