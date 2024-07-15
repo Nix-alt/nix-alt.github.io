ebcb9b6b3d22e50378b42312a35b3fa56b8c046e
Nix-alt <167569462+Nix-alt@users.noreply.github.com>
2024-07-15 15:01:07 -0400

    x

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 let x = "test";
 
-window.getElementByID('1').innerHTML = x;
+window.getElementById('1').innerHTML = x;
 setTimeout(function(){
     x = "\ttes"
+    window.getElementById('1').innerHTML = x;
 }, 2000);
 setTimeout(function(){
     x = "\tte"
-}, 2000);
+    window.getElementById('1').innerHTML = x;
+}, 2100);
 setTimeout(function(){
     x = "\tt"
-}, 2000);
+    window.getElementById('1').innerHTML = x;
+}, 2200);
 setTimeout(function(){
     x = "\t"
-}, 2000);
+    window.getElementById('1').innerHTML = x;
+}, 2300);
